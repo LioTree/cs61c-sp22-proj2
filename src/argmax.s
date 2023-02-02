8ce9ec1983ba352cd729e1046fba1db86c91c16e
LioTree <uc0110@163.com>
2023-02-02 12:18:39 +0800

    pass Task 3: Argmax

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,27 @@
 # =================================================================
 argmax:
 	# Prologue
-
-
+	li t0,1
+	blt a1,t0,error # size < 1?
+	li t0,0 # i = 0
+	li t5,0 # max_index=0
+	lw t6,0(a0) # max = a0[0]
 loop_start:
-
-
+	bge t0,a1,loop_end # foreach over?
+	slli t2,t0,2
+	add t3,t2,a0
+	lw t4,0(t3) # get element of a0
+	blt t4,t6,loop_continue
+	beq t4,t6,loop_continue
+	add t5,x0,t0
+	add t6,x0,t4
 loop_continue:
-
-
+    addi t0,t0,1 # i++
+	j loop_start
 loop_end:
 	# Epilogue
-
+	add a0,x0,t5
 	ret
+error:
+	li a0 36
+	jal exit
