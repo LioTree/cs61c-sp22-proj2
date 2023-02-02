032d25c496d8a023a7a7d741dad1e12fef833b7d
LioTree <uc0110@163.com>
2023-02-02 12:18:54 +0800

    pass Task 2: ReLU

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,22 @@
 # ==============================================================================
 relu:
 	# Prologue
-
-
+	li t0,1
+	blt a1,t0,error
+	li t0,0
 loop_start:
-
-
-
-
-
-
-
-
+	bge t0,a1,loop_end
+	slli t2,t0,2
+	add t3,t2,a0
+	lw t4,0(t3)
+	bge t4,x0,loop_continue
+	sw x0,0(t3)
 loop_continue:
-
-
-
+	addi t0,t0,1
+	j loop_start
 loop_end:
-
-
 	# Epilogue
-
-
 	ret
+error:
+	li a0 36
+	jal exit
