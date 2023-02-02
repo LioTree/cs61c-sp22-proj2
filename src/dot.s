e2c1726e0760efdcfde8d2b9459b3575b759e4c8
LioTree <uc0110@163.com>
2023-02-02 13:17:46 +0800

    pass Task 4: Dot Product

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,38 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
 	# Prologue
-
-
+	# check whether the length of the array is less than 1
+	li t0,1
+	blt a2,t0,Error36
+	blt a3,t0,Error37
+	blt a4,t0,Error37
+	li t0,0 # i = 0
+	li t1,0 # j = 0
+	li t3,0 # result = 0
 loop_start:
+	bge t0,a2,loop_end # i < a2?
+	slli t4,t0,2
+	add t4,t4,a0 # get &(a0[i])
+	lw t5,0(t4) # get value of a0[i]
 
+	slli t4,t1,2
+	add t4,t4,a1 # get &(a1[j])
+	lw t6,0(t4) # get value of a1[j]
 
+	mul t6,t5,t6
+	add t3,t3,t6
 
-
-
-
-
-
-
-
-
-
+	add t0,t0,a3 # i += a3
+	add t1,t1,a4 # j += a4
+	j loop_start
 loop_end:
-
-
 	# Epilogue
-
-
+	add a0,x0,t3
 	ret
+Error36:
+	li a0,36
+	jal exit
+Error37:
+	li a0,37
+	jal exit
